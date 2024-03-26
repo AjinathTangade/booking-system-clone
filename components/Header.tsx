56d76683e76e0dd6064b238b29d3f77f8dbdd82f
Ajinath Tangade <ajinathtangade12@gmail.com>
2024-03-26 15:08:27 +0530

    Header done with mobile responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,6 +14,8 @@ import {
   PhoneIcon,
 } from "@heroicons/react/24/solid";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { cn } from "@/lib/utils";
+
 const dropdownLink = [
   {
     name: "Book a Stay",
@@ -144,8 +146,7 @@ function Header() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed insert-0 z-10" />
-
-        <Dialog.Panel className="fixed insert-y-0 right-0 z-10 w-full overflow-y-auto bg-[#013B94] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#013B94] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className=" font-bold text-xl text-white">Booking.com</span>
@@ -156,8 +157,57 @@ function Header() {
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6 " aria-hidden="true"/>
+              <XMarkIcon className="h-6 w-6 " aria-hidden="true" />
             </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                        Stays
+                        <ChevronDownIcon
+                          className={cn(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...dropdownLink, ...callToAction].map((item) =>(
+                          <Disclosure.Button key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+            Flights
+          </a>
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+            Car Reantals
+          </a>
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+            Attractions
+          </a>
+          <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+            Flights + Hotel
+          </a>
+              </div>
+              <div className="lg:hidden  lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Log in <span> &rarr;</span>
+          </a>
+        </div>
+            </div>
           </div>
         </Dialog.Panel>
       </Dialog>
